8e1f6b49addb5ad700ee1f924439919b23fbb76e
Chisaneme Aloni <chisanemealoni@gmail.com>
2024-09-04 06:18:13 +0100

    modified version with new API

--- 06-Ajax_Type_Ahead/index.js
+++ 06-Ajax_Type_Ahead/index.js
@@ -1,41 +1,94 @@
-const endpoint =
-  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+// const endpoint =
+//   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
-const cities = [];
+// const cities = [];
+// fetch(endpoint)
+//   .then((blob) => blob.json())
+//   .then((data) => cities.push(...data));
+
+// function findMatches(wordToMatch, cities) {
+//   return cities.filter((place) => {
+//     // here we need to figure out if the city or state matches what was searched
+//     const regex = new RegExp(wordToMatch, "gi");
+//     return place.city.match(regex) || place.state.match(regex);
+//   });
+// }
+
+// function numberWithCommas(x) {
+//   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// }
+
+// function displayMatches() {
+//   const matchArray = findMatches(this.value, cities);
+//   const html = matchArray
+//     .map((place) => {
+//       const regex = new RegExp(this.value, "gi");
+//       const cityName = place.city.replace(
+//         regex,
+//         `<span class="hl">${this.value}</span>`
+//       );
+//       const stateName = place.state.replace(
+//         regex,
+//         `<span class="hl">${this.value}</span>`
+//       );
+//       return `
+//       <li>
+//         <span class="name">${cityName}, ${stateName}</span>
+//         <span class="population">${numberWithCommas(place.population)}</span>
+//       </li>
+//     `;
+//     })
+//     .join("");
+//   suggestions.innerHTML = html;
+// }
+
+// const searchInput = document.querySelector(".search");
+// const suggestions = document.querySelector(".suggestions");
+
+// searchInput.addEventListener("change", displayMatches);
+// searchInput.addEventListener("keyup", displayMatches);
+
+const endpoint =
+  "https://gist.githubusercontent.com/mykeels/1174cd68bcff6efc4f8cafb49a24a209/raw/388eea7fd85e2d615d92e473120355a0a37ab80b/states-and-cities.json";
+
+const statesAndCities = [];
+
 fetch(endpoint)
   .then((blob) => blob.json())
-  .then((data) => cities.push(...data));
+  .then((data) => statesAndCities.push(...data));
 
-function findMatches(wordToMatch, cities) {
-  return cities.filter((place) => {
-    // here we need to figure out if the city or state matches what was searched
+function findMatches(wordToMatch, statesAndCities) {
+  return statesAndCities.filter((place) => {
+    // Here we need to figure out if the cities or state matches what was searched (Changes from the original was made)
     const regex = new RegExp(wordToMatch, "gi");
-    return place.city.match(regex) || place.state.match(regex);
+    if (place.cities.some((city) => city.match(regex))) {
+      return true;
+    }
+    // Check if the state name matches the regex
+    return place.name.match(regex);
   });
 }
 
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
 function displayMatches() {
-  const matchArray = findMatches(this.value, cities);
+  const matchArray = findMatches(this.value, statesAndCities);
   const html = matchArray
     .map((place) => {
       const regex = new RegExp(this.value, "gi");
-      const cityName = place.city.replace(
+      // Assuming place.cities is an array of strings(Changes from the original was made)
+      const cityName = place.cities
+        .map((city) => {
+          return city.replace(regex, `<span class = "hl">${this.value}</span>`);
+        })
+        .join(", ");
+      // place.name is not an array but a string so it'll be directly written as:
+      const stateName = place.name.replace(
         regex,
-        `<span class="hl">${this.value}</span>`
-      );
-      const stateName = place.state.replace(
-        regex,
-        `<span class="hl">${this.value}</span>`
+        `<span class = "hl">${this.value}</span>`
       );
       return `
-      <li>
-        <span class="name">${cityName}, ${stateName}</span>
-        <span class="population">${numberWithCommas(place.population)}</span>
-      </li>
+    <li>
+      <span class="name">${cityName}, ${stateName}</span>
+    </li>
     `;
     })
     .join("");
